9cfe2de19040222c983197fb9658157e234b7387
Motto Man <mottoman1@users.noreply.github.com>
2017-04-17 14:56:20 +0200

    My first Commit for my first codelines

---
+++ oramain.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Created: 17.04.2017
+# Purpose: Main script
+
+
+[[ -z "$@" ]] && { echo "Call script with argument(s)!"; exit 1; }
+
+echo "Continue now..."; sleep 2
+# INITIALIZE % PREPARE
+
+# Main
+
+# Post actions
+
+# end of script
+
